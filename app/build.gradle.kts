93a7003853836a2d8218dcda5f64f1122143967e
srichey <richey32@gmail.com>
2026-04-17 09:39:57 -0700

    Add NDK debug symbol table to suppress Play Console native symbols warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
         debug {
             isMinifyEnabled = false
