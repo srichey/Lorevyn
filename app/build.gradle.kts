caf23e82d1783a85b20174216e334c534de96b81
srichey <richey32@gmail.com>
2026-04-17 09:35:30 -0700

    Bump versionCode to 7, fix R8 stripping @Serializable classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "com.lorevyn.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
