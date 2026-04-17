645c5f1e45b274e0b46108728f06638e621f8820
srichey <richey32@gmail.com>
2026-04-17 08:11:45 -0700

    Bump versionCode to 6 for internal test release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         applicationId = "com.lorevyn.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
+        versionCode = 6
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
